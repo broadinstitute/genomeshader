7ddfe618d46c6e5c3408555287ddba5d2e493cdc
Kiran V Garimella <kiran@broadinstitute.org>
2023-12-29 10:36:17 -0500

    Basics of a context menu implemented

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,22 @@
-use std::f32::INFINITY;
-
+use egui::{Pos2, Widget, Id};
 use nannou::{prelude::*, glam};
 use nannou_egui::*;
 
-use egui_dock::{DockArea, DockState, NodeIndex, Style};
-
 const GS_UI_BACKGROUND: rgb::Srgb<u8> = rgb::Srgb { red: 255, green: 255, blue: 255, standard: ::core::marker::PhantomData };
+const GS_UI_TEXT: rgb::Srgb<u8> = rgb::Srgb { red: 0, green: 0, blue: 0, standard: ::core::marker::PhantomData };
 const GS_UI_TRACK_1: rgb::Srgb<u8> = rgb::Srgb { red: 196, green: 209, blue: 217, standard: ::core::marker::PhantomData };
 const GS_UI_TRACK_2: rgb::Srgb<u8> = rgb::Srgb { red: 113, green: 131, blue: 143, standard: ::core::marker::PhantomData };
 
-struct TabViewer {}
-
-impl egui_dock::TabViewer for TabViewer {
-    type Tab = String;
-
-    fn title(&mut self, tab: &mut Self::Tab) -> egui::WidgetText {
-        (&*tab).into()
-    }
-
-    fn ui(&mut self, ui: &mut egui::Ui, tab: &mut Self::Tab) {
-        ui.label(format!("Content of {tab}"));
-    }
-}
-
 struct Settings {
-    resolution: u32,
-    scale: f32,
-    rotation: bool,
-    pan: bool,
-    last_position: Vec2,
-    position: Vec2,
+    pan: Vec2,
+    rotate: bool,
     stretch: f32,
     zoom: f32,
+
     show_settings: bool,
-    tree: DockState<String>,
+
+    show_popup: bool,
+    pos_popup: Pos2,
 }
 
 struct Model {
@@ -45,6 +28,11 @@ fn raw_window_event(_app: &App, model: &mut Model, event: &nannou::winit::event:
     // Let egui handle things like keyboard and mouse input.
     model.egui.handle_raw_event(event);
 
+    handle_zoom(event, model);
+    handle_hotkeys(event, model);
+}
+
+fn handle_zoom(event: &nannou::winit::event::WindowEvent<'_>, model: &mut Model) {
     // Handle mouse wheel events
     if let nannou::winit::event::WindowEvent::MouseWheel { delta, .. } = event {
         match delta {
@@ -59,42 +47,37 @@ fn raw_window_event(_app: &App, model: &mut Model, event: &nannou::winit::event:
         }
     }
 
-    // Handle mouse drag events for panning
-    if let nannou::winit::event::WindowEvent::MouseInput { state, button, .. } = event {
-        if *state == nannou::winit::event::ElementState::Pressed && *button == nannou::state::mouse::Button::Left {
-            model.settings.pan = true;
-        }
-
-        if *state == nannou::winit::event::ElementState::Released && *button == nannou::state::mouse::Button::Left {
-            model.settings.pan = false;
-        }
-    }
-
-    if model.settings.pan {
-        if let nannou::winit::event::WindowEvent::CursorMoved { position, .. } = event {
-            let this_position = vec2(position.x as f32, -position.y as f32);
-
-            if model.settings.last_position.x != INFINITY {
-                let diff_position = this_position - model.settings.last_position;
-                model.settings.position += diff_position/5.0;
+    // Capture Command+Plus and Command+Minus keyboard shortcuts
+    if let nannou::winit::event::WindowEvent::KeyboardInput { input, .. } = event {
+        if let Some(nannou::winit::event::VirtualKeyCode::Equals) = input.virtual_keycode {
+            if input.state == nannou::winit::event::ElementState::Pressed && model.egui.ctx().input(|i| i.modifiers.command) {
+                model.settings.zoom *= 1.2; // adjust zoom factor as needed
             }
+        }
 
-            model.settings.last_position = this_position;
+        if let Some(nannou::winit::event::VirtualKeyCode::Minus) = input.virtual_keycode {
+            if input.state == nannou::winit::event::ElementState::Pressed && model.egui.ctx().input(|i| i.modifiers.command) {
+                model.settings.zoom *= 0.8; // adjust zoom factor as needed
+            }
         }
     }
+}
 
+fn handle_hotkeys(event: &nannou::winit::event::WindowEvent<'_>, model: &mut Model) {
     // Capture keypress for 'S' and toggle model.settings.show_settings
     if let nannou::winit::event::WindowEvent::KeyboardInput { input, .. } = event {
-        if let Some(nannou::winit::event::VirtualKeyCode::S) = input.virtual_keycode {
-            if input.state == nannou::winit::event::ElementState::Pressed {
-                model.settings.show_settings = !model.settings.show_settings;
-            }
-        }
-
-        if let Some(nannou::winit::event::VirtualKeyCode::R) = input.virtual_keycode {
-            if input.state == nannou::winit::event::ElementState::Pressed {
-                model.settings.rotation = !model.settings.rotation;
-            }
+        match input.virtual_keycode {
+            Some(nannou::winit::event::VirtualKeyCode::S) => {
+                if input.state == nannou::winit::event::ElementState::Pressed {
+                    model.settings.show_settings = !model.settings.show_settings;
+                }
+            },
+            Some(nannou::winit::event::VirtualKeyCode::R) => {
+                if input.state == nannou::winit::event::ElementState::Pressed {
+                    model.settings.rotate = !model.settings.rotate;
+                }
+            },
+            _ => {}
         }
     }
 }
@@ -112,21 +95,17 @@ fn model(app: &App) -> Model {
     let window = app.window(window_id).unwrap();
 
     let egui = Egui::from_window(&window);
-    let tree = DockState::new(vec!["Tab1".to_owned()]);
 
     Model {
         egui,
         settings: Settings {
-            resolution: 10,
-            scale: 200.0,
-            rotation: true,
-            pan: false,
-            last_position: vec2(INFINITY, INFINITY),
-            position: vec2(0.0, 0.0),
+            pan: vec2(0.0, 0.0),
+            rotate: true,
             stretch: 1.0,
             zoom: 1.0,
             show_settings: false,
-            tree: tree
+            show_popup: false,
+            pos_popup: Pos2::new(0.0, 0.0)
         },
     }
 }
@@ -135,28 +114,33 @@ fn view(app: &App, model: &Model, frame: Frame) {
     let settings = &model.settings;
 
     let transform = glam::Mat4::from_scale_rotation_translation(
-        glam::Vec3::new(settings.zoom, settings.zoom, 1.0), // scale
-        if settings.rotation {
+        glam::Vec3::new(settings.zoom, settings.zoom, 1.0),
+        if settings.rotate {
             glam::Quat::IDENTITY
         } else {
             glam::Quat::from_rotation_z(std::f32::consts::FRAC_PI_2)
         },
-        glam::Vec3::new(settings.position.x, settings.position.y, 0.0) // translation
+        glam::Vec3::new(settings.pan.x, settings.pan.y, 0.0)
     );
 
     let draw = app
         .draw()
-        .scale_x(if settings.rotation { settings.stretch } else { 1.0 })
-        .scale_y(if settings.rotation { 1.0 } else { settings.stretch })
         .transform(transform);
 
     draw.background().color(GS_UI_BACKGROUND);
 
+    draw.text("Sample 1")
+        .color(GS_UI_TEXT)
+        .center_justify()
+        .font_size(10)
+        .x(-50.0)
+        .y(0.0);
+
     draw.rect()
         .stroke_weight(1.0)
         .caps_round()
-        .x(settings.position.x)
-        .y(settings.position.y)
+        .x(200.0)
+        .y(0.0)
         .width(400.0)
         .height(10.0)
         .color(GS_UI_TRACK_1);
@@ -164,8 +148,8 @@ fn view(app: &App, model: &Model, frame: Frame) {
     draw.rect()
         .stroke_weight(1.0)
         .caps_round()
-        .x(settings.position.x)
-        .y(settings.position.y + 15.0)
+        .x(200.0)
+        .y(15.0)
         .width(400.0)
         .height(10.0)
         .color(GS_UI_TRACK_2);
@@ -183,42 +167,50 @@ fn update(app: &App, model: &mut Model, update: Update) {
     egui.set_elapsed_time(update.since_start);
     let ctx = egui.begin_frame();
 
-    egui::TopBottomPanel::top("header").show(&ctx, |ui| {
-        ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
-            if ui.small_button("Rotate").clicked() {
-                settings.rotation = !settings.rotation;
-            }
-
-            if ui.small_button("+").clicked() {
-                let tab_id = format!("Tab{}", settings.tree.main_surface().num_tabs() + 1);
-                if let Some(root_node) = settings.tree.main_surface_mut().root_node_mut() {
-                    root_node.append_tab(tab_id.to_owned());
-                } else {
-                    settings.tree = DockState::new(vec!["Tab1".to_owned()]);
-                }
-            }
-        });
-    });
-
     egui::TopBottomPanel::bottom("footer").show(&ctx, |ui| {
-        ui.label("Status");
+        ui.label(format!("Status: {:?} {:?}", app.mouse.position(), app.mouse.buttons));
     });
 
-    egui::CentralPanel::default().show(&ctx, |ui| {
-        DockArea::new(&mut settings.tree)
-            .style(Style::from_egui(ctx.style().as_ref()))
-            .show(&ctx, &mut TabViewer {});
-    });
+    match app.mouse.buttons.right().if_down() {
+        Some(mouse_pos) => {
+            // egui::show_tooltip_text(&ctx, egui::Id::new("my_tooltip"), "Hello");
+            // egui::show_tooltip_at_pointer(&ctx, egui::Id::new("my_tooltip"), |ui| {});
+
+            if !settings.show_popup {
+                settings.show_popup = true;
+                settings.pos_popup = Pos2::new(
+                    mouse_pos.x + (app.window_rect().w()/2.0),
+                    (app.window_rect().h()/2.0) - mouse_pos.y
+                );
+            }
+        },
+        None => {},
+    }
+
+    if settings.show_popup {
+        egui::Area::new("menu")
+            .fixed_pos(settings.pos_popup)
+            .show(&ctx, |ui| {
+            if ui.button("Close").clicked() {
+                settings.show_popup = false;
+            };
+        });
+    }
 
     if settings.show_settings {
         egui::Window::new("Settings").show(&ctx, |ui| {
-            // Resolution slider
-            ui.label("Resolution:");
-            ui.add(egui::Slider::new(&mut settings.resolution, 1..=40));
+            // Pan X slider
+            ui.label("Pan X:");
+            ui.add(egui::Slider::new(&mut settings.pan.x, -1000.0..=1000.0));
 
-            // Scale slider
-            ui.label("Scale:");
-            ui.add(egui::Slider::new(&mut settings.scale, 0.0..=1000.0));
+            // Pan Y slider
+            ui.label("Pan Y:");
+            ui.add(egui::Slider::new(&mut settings.pan.y, -1000.0..=1000.0));
+
+            if ui.button("reset").clicked() {
+                settings.pan.x = 0.0;
+                settings.pan.y = 0.0;
+            }
         });
     }
 }
